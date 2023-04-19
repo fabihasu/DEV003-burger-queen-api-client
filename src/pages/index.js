61f0fb8e2b0b6c1539a2a53fba6920b7237a1f4e
Luisa <lorenitaa889@gmail.com>
2023-04-19 16:42:44 -0500

    Comenzando vista de mesero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './styles/Home.module.css'
-
+// vista del login
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
--- src/pages/waiter.js
+++ src/pages/waiter.js
@@ -1,7 +1,20 @@
+import style from './styles/Home.module.css'
 export default function waiter() {
     return (
         <>
-        <p>Hola mundo</p>
+        <header className={style.card}>
+          <div>
+          <img
+            src="https://github.com/Laboratoria/burger-queen-api-mock/blob/main/resources/images/coffee.png?raw=true"
+            alt= "Imagen"
+            width={150}
+            height={150}
+          />
+          <h1>BURGER QUEEN</h1>
+             <button>Cocina</button>
+            <button>Órdenes</button>
+          </div>
+        </header>
         </>
     )
 }
