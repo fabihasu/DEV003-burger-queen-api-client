38fe9272cfd38c4d5405e5c0a5a489091e32fc05
Fabiana Hasu <fabi.hasu@gmail.com>
2023-05-02 23:42:45 -0300

    actualizar lista de ordenes

--- src/pages/components_waiter/orderContext.js
+++ src/pages/components_waiter/orderContext.js
@@ -3,8 +3,7 @@ import { createContext, useState } from "react";
 export const OrderContext = createContext();
 
 export const OrderProvider = ({children}) => {
-    const [order, setOrder] = useState({})
-
+    const [order, setOrder] = useState([])
     return (
         <OrderContext.Provider value={[order, setOrder]}>
             {children}
